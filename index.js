d3bcfb009a40fb2137f0dba338872593102580f1
Nesaku <78386413+nesaku@users.noreply.github.com>
2025-10-30 05:33:18 +0000

    add glow effects

--- index.js
+++ index.js
@@ -2,16 +2,3 @@ console.log(
   "%c".concat("Built By: Nesaku | https://github.com/nesaku"),
   "color:green"
 );
-
-  document.addEventListener("DOMContentLoaded", () => {
-    const img = document.getElementById("glow-img");
-    if (!img) return;
-
-    img.addEventListener("mousemove", (e) => {
-      const rect = img.getBoundingClientRect();
-      const x = ((e.clientX - rect.left) / rect.width) * 100;
-      const y = ((e.clientY - rect.top) / rect.height) * 100;
-      img.style.setProperty("--x", `${x}%`);
-      img.style.setProperty("--y", `${y}%`);
-    });
-  });
